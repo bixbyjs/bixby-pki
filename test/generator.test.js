6ab4883ef1177b5635646d44fecb8a602e792de2
Jeremie Miller <jeremie@jabber.org>
2014-09-12 12:53:09 -0600

    add configurable key id naming to generation component

--- test/generator.test.js
+++ test/generator.test.js
@@ -16,6 +16,28 @@ describe('bixby-pki/generator', function() {
       expect(pair).to.be.an('object');
       expect(pair.public).to.be.a('string');
       expect(pair.private).to.be.a('string');
+      expect(pair.kid).to.be.a('string');
+      expect(pair.kid.length).to.be.equal(8);
+      done();
+    });
+  });
+
+  it('should config hourly kids', function(done) {
+    var factory = pkg('generator');
+    var gen = factory({get:function(){return {naming:'hour'};}});
+    gen(function(err,pair){
+      expect(err).to.not.exist;
+      expect(pair.kid.length).to.be.equal(11);
+      done();
+    });
+  });
+
+  it('should config epoch kids', function(done) {
+    var factory = pkg('generator');
+    var gen = factory({get:function(){return {naming:'epoch'};}});
+    gen(function(err,pair){
+      expect(err).to.not.exist;
+      expect(pair.kid.length).to.be.equal(10);
       done();
     });
   });
