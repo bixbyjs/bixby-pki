1d08e8d5e5d922a29b79881abf9dc5c8906e2a22
Jared Hanson <jaredhanson@gmail.com>
2016-10-25 11:17:53 -0700

    Add tests.

--- test/package.test.js
+++ test/package.test.js
@@ -4,8 +4,28 @@ var pkg = require('..');
 
 describe('bixby-pki', function() {
   
-  it('should export object', function() {
-    expect(pkg).to.be.a('function');
+  it('should export manifest', function() {
+    expect(pkg).to.be.an('object');
+    expect(pkg['']).to.be.a('function');
+    expect(pkg['ca']).to.be.a('function');
+  });
+  
+  describe('pki', function() {
+    var main = pkg[''];
+    
+    it('should be annotated', function() {
+      expect(main['@implements']).to.equal('http://i.bixbyjs.org/pki');
+      expect(main['@singleton']).to.equal(true);
+    });
+  });
+  
+  describe('pki/ca', function() {
+    var ca = pkg['ca'];
+    
+    it('should be annotated', function() {
+      expect(ca['@implements']).to.equal('http://i.bixbyjs.org/pki/CertificateAuthority');
+      expect(ca['@singleton']).to.equal(true);
+    });
   });
   
 });
